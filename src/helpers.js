23f503473b11a4fc433b2e3fa6e4094d98e846d2
Darpan Sagpariya <79413915+darpan211@users.noreply.github.com>
2025-06-18 19:52:03 +0530

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-// Utility function to capitalize the first letter of each word
+// Utility function to capitalize the first letter of each word.
 export function toCapitalize(str) {
   return str.replace(/\w\S*/g, (txt) =>
     txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
